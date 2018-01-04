f718e556ecfe417b1821ea0ed777572cba0d4448
zhangshujuan <zhangshujuan@renhenet.com.cn>
2018-01-04 16:28:12 +0800

    no message

---
+++ dev/static/script/plugins/bs/index.js
@@ -0,0 +1,9 @@
+define(function(require, exports, module) {
+	require("./transition");
+	require("./popover");
+	
+	require("./Affix");
+	require("./collapse");
+	require("./modal");
+	require("./scrollspy");
+})
